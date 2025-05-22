53cdbcd7ef877b3c3fa089e931a361af30027004
Tyerone <141754956+Pro5essorE@users.noreply.github.com>
2025-05-22 09:55:52 -0500

    Update Display.java

--- Display.java
+++ Display.java
@@ -17,37 +17,37 @@ public class Display extends JFrame{
       setTitle(player);
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       setLayout(new BorderLayout());
-      
+     
       playerGrid = new Grid(10, 10);
       targetGrid = new Grid(10, 10);
-      
+     
       JPanel gridsPanel = new JPanel();
       gridsPanel.setLayout(new BoxLayout(gridsPanel, BoxLayout.Y_AXIS));
-      gridsPanel.setBackground(ColorPalette.BACKGROUND);
-      
+      gridsPanel.setBackground(ColorPalette.getBackgroundColor());
+     
       gridsPanel.add(targetGrid);
       gridsPanel.add(Box.createRigidArea(new Dimension(0, 10)));
       gridsPanel.add(playerGrid);
       this.add(gridsPanel, BorderLayout.CENTER);
-      
+     
       JButton rotateButton = new JButton("Rotate (" +currentDirection + ")");
       rotateButton.addActionListener(e -> {
          currentDirection = currentDirection.next();
          rotateButton.setText("Rotate (" + currentDirection + ")");
       });
-      
+     
       shipLengthLabel = new JLabel("-");
       shipLengthLabel.setHorizontalAlignment(SwingConstants.CENTER);
-      
+     
       JPanel controlPanel = new JPanel(new BorderLayout());
       controlPanel.add(shipLengthLabel, BorderLayout.CENTER);
       controlPanel.add(rotateButton, BorderLayout.EAST);
-      
+     
       this.add(controlPanel, BorderLayout.SOUTH);
-      
+     
       this.pack();
       this.setVisible(true);
-      
+     
       setupShipStorage();
    }
    
@@ -57,7 +57,7 @@ public class Display extends JFrame{
       shipsToPlace.add(new Battleship("Cruiser", 3));
       shipsToPlace.add(new Battleship("Submarine", 3));
       shipsToPlace.add(new Battleship("Destroyer", 2));
-      
+     
       shipLengthLabel.setText("" + shipsToPlace.get(0).getLength());
       enablePlacementMode();
    }
@@ -75,17 +75,17 @@ public class Display extends JFrame{
    
    private void handleShipPlacement(int startRow, int startCol){
       if(shipsToPlace.isEmpty()) return;
-      
+     
       int length = shipsToPlace.get(0).getLength();
-      
+     
       if(!playerGrid.canPlaceShip(startRow, startCol, length, currentDirection)){
          System.out.println("Invalid ship placement.");
          return;
       }
-      
+     
       playerGrid.placeShipTiles(startRow, startCol, length, currentDirection, shipsToPlace.get(0));
       shipsToPlace.remove(0);
-      
+     
       if(shipsToPlace.isEmpty()){
          placementPhase = false;
          shipLengthLabel.setText("-");
