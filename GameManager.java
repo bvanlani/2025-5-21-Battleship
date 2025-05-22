ba62344ecd42f5e9f5f1239e3cc0427f85d96c9a
Toryn <torynlb@gmail.com>
2025-05-22 10:01:33 -0500

    Update GameManager.java

--- GameManager.java
+++ GameManager.java
@@ -12,11 +12,17 @@ public class GameManager{
    //Contains a boolean variable that represents whether or not the game is running.
    private boolean gameRunning;
    
+   //Should only contain a '1' or a '2', with the numbers representing which player's turn it is.
+   private int currentTurn;
+   
    private Player player1; 
    
    private Player player2;
    
-   private Display gameDisplay;
+   private Display player1Display;
+   
+   private Display player2Display;
+   
    
    //Contains a boolean variable, which represents whether either of the players have had all their ships sunk or not.
    private boolean isGameOver;
@@ -28,24 +34,26 @@ public class GameManager{
     * @param player2 The second player that will be participating in the game.
     * @param display The window where Battleship will be played.
     */
-   public GameManager(Player player1, Player player2, Display display){
+   public GameManager(Player player1, Player player2, Display display1, Display display2){
       gameRunning = false;
       
       this.player1 = player1;
       this.player2 = player2;
       
+      player1Display = display1;
+      player2Display = display2;
+      
       currentTurn = 1;
       
       isGameOver = false;
       
-      gameDisplay = display;
    }
    
    
    /*
     * Updates the currentTurn object, so that it changes to either 1 or 2, depending on which player's turn it is about to be. - Toryn
     */
-   public void switchTurns(){
+   private void switchTurns(){
       if(player1.getIsTurn()){
          player1.setTurn(false);
          player2.setTurn(true);
@@ -61,7 +69,7 @@ public class GameManager{
     * If there is at least one ship that has not been sunk yet, the code will return false. Otherwise, the code will return true. - Toryn
     *
     */
-   public boolean checkForGameOver(){
+   private boolean checkForGameOver(){
       if(currentTurn == 1){
          for(Battleship s : player1.getFleet()){ 
             if(s.checkIsSunk() == false){
@@ -79,11 +87,11 @@ public class GameManager{
       return true;
    }
    
-   public void player1Turn(){
-      
+   private void player1Turn(){
+      player1Display.setVisible(true);
    }
    
-   public void player2Turn(){
+   private void player2Turn(){
    
    }
    
@@ -91,10 +99,9 @@ public class GameManager{
     * Starts the game of Battleship.
     */
    public void startGame(){
-   
       while(!(isGameOver)){
-      
-      }
+       
+       }
    }
    
 }
