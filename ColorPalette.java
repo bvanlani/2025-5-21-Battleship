ad979b5f89faddcdde52f1931baacaf99e88344c
cmoney52 <149429981+cmoney52@users.noreply.github.com>
2025-05-22 09:37:17 -0500

    Color Pallet Update
    Colors are now private and are read only with getters.

--- ColorPalette.java
+++ ColorPalette.java
@@ -1,9 +1,37 @@
 import java.awt.Color;
 
 public final class ColorPalette{
-   public static final Color SHIP = new Color(102, 102, 102);
-   public static final Color HITSHIP = new Color(154, 51, 52);
-   public static final Color WATER = new Color(145, 195, 220);
-   public static final Color FLAG = new Color(234, 238, 234);
-   public static final Color BACKGROUND = new Color(52, 68, 64);
+   private static final Color SHIP = new Color(102, 102, 102);
+   private static final Color HITSHIP = new Color(154, 51, 52);
+   private static final Color WATER = new Color(145, 195, 220);
+   private static final Color FLAG = new Color(234, 238, 234);
+   private static final Color BACKGROUND = new Color(52, 68, 64);
+
+   // Accessor Methods
+
+   // Get Ship color
+   public static Color getShipColor(){
+      return SHIP;
+   }
+
+   // Get HitShip color
+   public static Color getHitShipColor(){
+      return HITSHIP;
+   }
+
+   // Get Water color
+   public static Color getWaterColor(){
+      return WATER;
+   }
+
+   // Get Flag color
+   public static Color getFlagColor(){
+      return FLAG;
+   }
+
+   // Get Background color
+   public static Color getBackgroundColor(){
+      return BACKGROUND;
+   }
+
 }
